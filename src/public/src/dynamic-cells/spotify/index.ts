5311aed97ef4fa8ed14cb8435d013ac4d7a56315
loloToster <lolo.toster4@gmail.com>
2022-07-21 21:33:14 +0200

    🚧 added base for player in spotify cell

--- src/public/src/dynamic-cells/spotify/index.ts
+++ src/public/src/dynamic-cells/spotify/index.ts
@@ -1,7 +1,20 @@
 const playerEl = document.querySelector<HTMLDivElement>(".player")!
 
 const switchToPlayer = document.querySelector<HTMLButtonElement>(".nav__player")!
+const switchToMenu = document.querySelector<HTMLButtonElement>(".player__back")!
+
+document.querySelectorAll<HTMLInputElement>(".spotify-input").forEach(i => {
+    i.addEventListener("input", () => {
+        const v = parseInt(i.value)
+        const max = parseInt(i.max)
+        i.style.setProperty("--percentage", ((v / max) * 100).toString())
+    })
+})
 
 switchToPlayer.addEventListener("click", () => {
     playerEl.classList.add("active")
+})
+
+switchToMenu.addEventListener("click", () => {
+    playerEl.classList.remove("active")
 })
