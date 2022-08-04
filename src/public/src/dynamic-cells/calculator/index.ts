08c146ae9936ac47cfc9f0774c14dc1afcb97720
loloToster <lolo.toster4@gmail.com>
2022-08-04 18:28:08 +0200

    ✨ added basic functionality to calculator cell

---
+++ src/public/src/dynamic-cells/calculator/index.ts
@@ -0,0 +1,60 @@
+import { evaluate } from "mathjs"
+
+const input = document.querySelector<HTMLInputElement>(".calc__input")!
+const clearInput = document.querySelector<HTMLElement>(".calc__clear")!
+const resultBox = document.querySelector<HTMLDivElement>(".calc__result")!
+
+function toggleClearBtn() {
+    clearInput.classList.toggle("active", Boolean(input.value))
+}
+
+function onInput() {
+    toggleClearBtn()
+
+    let result: string
+
+    try {
+        let expression = input.value.replace(/π/g, ` ${Math.PI.toString()} `)
+        let evaluation: string | undefined = evaluate(expression)
+        if (evaluation) {
+            result = "= " + evaluation
+        } else
+            result = ""
+    } catch {
+        result = "Invalid Syntax"
+    }
+
+    resultBox.innerText = result
+}
+
+input.addEventListener("input", () => onInput())
+
+clearInput.addEventListener("click", () => {
+    input.value = ""
+    toggleClearBtn()
+    onInput()
+})
+
+const operators = [
+    {
+        class: "ce", action: () => {
+            input.value = input.value.slice(0, -1)
+            toggleClearBtn()
+            onInput()
+        }
+    },
+    { class: "root", action: () => { } },
+    {
+        class: "pi", action: () => {
+            input.value += "π"
+            onInput()
+            input.focus()
+        }
+    },
+    { class: "pow", action: () => { } }
+]
+
+operators.forEach(op => {
+    document.querySelector<HTMLButtonElement>(`.calc__${op.class}`)
+        ?.addEventListener("click", () => op.action())
+})
