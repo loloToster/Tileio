c8a7f512c5b5f7194e41aefc97b02caf698555f7
loloToster <lolo.toster4@gmail.com>
2022-08-05 13:42:33 +0200

    🔧 added github redirect env variable

--- src/config/passport-config.ts
+++ src/config/passport-config.ts
@@ -15,6 +15,7 @@ passport.deserializeUser(async (id, done) => {
 })
 
 type parserFunc = (profile: any, sId: string) => Omit<IUser, "grid" | "dynamicCells">
+
 async function verify(profile: any, done: Function, strategy: string, parser: parserFunc) {
     const strategyId = `${strategy}-${profile.id}`
 
@@ -32,33 +33,37 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     callbackURL: "/auth/google/callback",
     scope: ["profile", "email"]
-}, async (aT, rT, prof, cb) => verify(prof, cb, "ggl", (p, sId) => {
-    return {
-        name: p.displayName,
-        strategyId: sId,
-        email: p._json.email || "",
-        picture: p._json.picture || ""
-    }
-})))
+}, async (aT, rT, prof, cb) =>
+    verify(prof, cb, "ggl", (p, sId) => {
+        return {
+            name: p.displayName,
+            strategyId: sId,
+            email: p._json.email || "",
+            picture: p._json.picture || ""
+        }
+    })
+))
 
 passport.use(new DiscordStrategy({
     clientID: process.env.DISCORD_CLIENT_ID!,
     clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     callbackURL: "/auth/discord/callback",
     scope: ["identify", "email"]
-}, async (aT, rT, prof, cb) => verify(prof, cb, "dsc", (p, sId) => {
-    return {
-        name: p.username,
-        strategyId: sId,
-        email: p.email || "",
-        picture: p.avatar ? `https://cdn.discordapp.com/avatars/${p.id}/${p.avatar}.png` : ""
-    }
-})))
+}, async (aT, rT, prof, cb) =>
+    verify(prof, cb, "dsc", (p, sId) => {
+        return {
+            name: p.username,
+            strategyId: sId,
+            email: p.email || "",
+            picture: p.avatar ? `https://cdn.discordapp.com/avatars/${p.id}/${p.avatar}.png` : ""
+        }
+    })
+))
 
 passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID!,
     clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    callbackURL: "/auth/github/callback",
+    callbackURL: (process.env.GITHUB_REDIRECT || "") + "/auth/github/callback",
     scope: ["read:user", "user:email"]
 }, async (aT: string, rT: string, prof: any, cb: Function) =>
     verify(prof, cb, "git", (p, sId) => {
