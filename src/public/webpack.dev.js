e9ece10cacba788d7b0836925a3f3d89dd7f863a
loloToster <lolo.toster4@gmail.com>
2023-10-14 15:11:16 +0200

    🎉 initial nextjs setup

--- src/public/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require("webpack-merge")
-const common = require("./webpack.common")
-
-// compile typescript
-module.exports = merge(common, {
-    mode: "development",
-    devtool: false,
-    watch: true
-})
